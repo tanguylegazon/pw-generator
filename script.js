8534c7ff875c71c691966a6e92a5b0bd0e3eaf03
Tanguy <eseltwift@gmail.com>
2024-12-22 13:30:09 +0100

    Add password.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+// import * as password from './password.js';
+
 /**
  * @constant {number} minPasswordLength
  * @description The minimum length that a password can be.
