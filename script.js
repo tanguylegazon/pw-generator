617d5f0379d38231fda066405cd2787bf5979f64
Tanguy <eseltwift@gmail.com>
2026-03-08 15:06:26 +0100

    Change ambiguousCharset

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ const digitCharset = "0123456789";
 const lowerCaseCharset = "abcdefghijklmnopqrstuvwxyz";
 const upperCaseCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const symbolCharset = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-const ambiguousCharset = "012CcIilOoXxZz!\"$&'+,/:;@[\\]^`{|}~";
+const ambiguousCharset = "012CcIilOoXxZz!\"$&'+,/:;[\\]^`{|}~";
 
 /**
  * @function updateCharset
